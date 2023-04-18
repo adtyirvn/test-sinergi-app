a6c1ab4eeceb5af87c9d5962e963c146c64cc819
Aditya Irvan <adityairvanku@gmail.com>
2023-04-18 09:50:38 +0700

    [fix] todolist index

--- resources/views/todolists/index.blade.php
+++ resources/views/todolists/index.blade.php
@@ -11,8 +11,6 @@
   <body class="bg-light">
     <main class="container">
         @include('component.menu')
-        @extends('layout.template')
-        @section('konten')
         <!-- START DATA -->
         <div class="my-3 p-3 bg-body rounded shadow-sm">
                 <!-- TOMBOL TAMBAH DATA -->
